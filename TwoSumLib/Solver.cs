06fb8bbae636a4a04a1c809fd2092e336ab2149a
MikailAbyshov <142107917+MikailAbyshov@users.noreply.github.com>
2024-06-27 10:46:55 +0300

    Add files via upload

---
+++ TwoSumLib/Solver.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoSum_Solver;
+
+public class Solver
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] resultingArray = new int[2];
+
+        for (var pivotIndex = 0; pivotIndex < nums.Length; pivotIndex++)
+        {
+
+            var pairIndex = Array.IndexOf(nums, target - nums[pivotIndex]);
+
+            if (pairIndex >= 0 && pairIndex != pivotIndex)
+            {
+                resultingArray[0] = pivotIndex;
+                resultingArray[1] = pairIndex;
+                return resultingArray;
+            }
+        }
+        return resultingArray;
+    }
+}
