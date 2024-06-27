70dcad97ec7588eb388822553259dc05ded4678f
MikailAbyshov <142107917+MikailAbyshov@users.noreply.github.com>
2024-06-27 10:48:50 +0300

    Add files via upload

---
+++ TwoSumTests/UnitTestsForTwoSum.cs
@@ -0,0 +1,27 @@
+using TwoSum_Solver;
+using Xunit.Sdk;
+
+namespace TwoSum_Tests;
+
+public class UnitTestsForTwoSum
+{
+    private readonly IEnumerable<object> DataForUnitTests = new List<object>
+    {
+         new object[] {new[] { 2, 7, 11, 15 }, new[] { 0, 1 }, 9 },
+         new object[] {new[] { 2, 7, 11, 15 }, new[] { 0, 1 }, 9 },
+    };
+
+
+    [Theory]
+    [MemberData(nameof(DataForUnitTests))]
+
+    public void UnitTestForSolverByEqualistaion(int[] numbers, int[] expected, int target)
+    {
+        Solver solver = new Solver();
+        int[] actual = new int[2];
+
+        actual = solver.TwoSum(numbers, target);
+
+        Assert.Equal(expected, actual);
+    }
+}
